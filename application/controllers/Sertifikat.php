2fcf45abfeed10114334b7fe95d6f87c8cf60898
roem9 <muhammadrum04@gmail.com>
2023-08-15 20:26:53 +0700

    sertifikat access

--- application/controllers/Sertifikat.php
+++ application/controllers/Sertifikat.php
@@ -33,7 +33,7 @@ class Sertifikat extends CI_Controller {
             $peserta['skor'] = $skor;
 
             // $peserta['no_doc'] = "{$peserta['no_doc']}/TOAFL/ACP/{$peserta['bulan']}/{$peserta['tahun']}";
-            $peserta['no_doc'] = "{$peserta['tahun']}/{$peserta['no_doc']}";
+            $peserta['no_doc'] = "AC/ES/{$peserta['tahun']}/1{$peserta['no_doc']}";
         }
 
         // $this->load->view("pages/layout/header-sertifikat", $peserta);
